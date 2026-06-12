abfea24450f0305037e5cd56a1401c606f523c0f
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-06-12 18:18:51 +0200

    Removed ../ from path

--- js/images.js
+++ js/images.js
@@ -1,31 +1,31 @@
 export const images = [
   {
-    src: "../bilder/band-cover-photo.jpg",
+    src: "bilder/band-cover-photo.jpg",
     alt: "Blått og rått medlemmer ved sjøen.",
     featured: true,
   },
   {
-    src: "../bilder/band-cover-press.jpg",
+    src: "bilder/band-cover-press.jpg",
     alt: "Blått og rått medlemmer ved sjøen i svart og hvitt.",
     featured: true,
   },
   {
-    src: "../bilder/band-members-s.JPG",
+    src: "bilder/band-members-s.JPG",
     alt: "Noen av Blått og rått sine medlemmer.",
     featured: true,
   },
   {
-    src: "../bilder/band-on-big-stage.jpeg",
+    src: "bilder/band-on-big-stage.jpeg",
     alt: "Blått og rått på stor scene.",
     featured: true,
   },
   {
-    src: "../bilder/band-on-medium-stage.jpeg",
+    src: "bilder/band-on-medium-stage.jpeg",
     alt: "Blått og rått på scenen.",
     featured: true,
   },
   {
-    src: "../bilder/band-on-small-stage.jpeg",
+    src: "bilder/band-on-small-stage.jpeg",
     alt: "Blått og rått på liten scene.",
     featured: true,
   },
