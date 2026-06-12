f2f71249ceb0cca5114efbb6fd471d5cddab0aee
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-06-12 18:12:35 +0200

    Edited capital J to j

--- js/concerts.js
+++ js/concerts.js
@@ -1,12 +1,12 @@
 export let concerts = [
   {
-    date: "20. Juni 2026",
+    date: "20. juni 2026",
     name: "Lyngenfjord live",
     place: "Lyngen",
     tickets: "https://tikkio.com/events/62885-lyngenfjord-live-ivgobahta",
   },
   {
-    date: "25. Juni 2026",
+    date: "25. juni 2026",
     name: "Finnsnes i fest",
     place: "Finnsnes",
     tickets: "https://fifest.no/billetter-",
