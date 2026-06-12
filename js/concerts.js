3d942ad2a00baa7c99629562e3620c90e31c2e91
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-06-12 15:30:28 +0200

    Created concerts array

---
+++ js/concerts.js
@@ -0,0 +1,14 @@
+export let concerts = [
+  {
+    date: "20. Juni 2026",
+    name: "Lyngenfjord live",
+    place: "Lyngen",
+    tickets: "https://tikkio.com/events/62885-lyngenfjord-live-ivgobahta",
+  },
+  {
+    date: "25. Juni 2026",
+    name: "Finnsnes i fest",
+    place: "Finnsnes",
+    tickets: "https://fifest.no/billetter-",
+  },
+];
