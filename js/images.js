26fd5e7a5d2b2e8b9c41a69e1333641e1944b08d
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-06-20 13:27:45 +0200

    Added images to array

--- js/images.js
+++ js/images.js
@@ -1,4 +1,5 @@
 export const images = [
+  // FEATURED
   {
     src: "./bilder/band-cover-photo.jpg",
     alt: "Blått og rått medlemmer ved sjøen.",
@@ -28,5 +29,81 @@ export const images = [
     src: "./bilder/band-on-small-stage.jpeg",
     alt: "Blått og rått på liten scene.",
     featured: true,
+  },
+  {
+    src: "./bilder/bassist-ronald.jpg",
+    alt: "Blått og rått bassist Ronald Alertsen.",
+    featured: true,
+  },
+  {
+    src: "./bilder/bogr-mike-farris.jpg",
+    alt: "Blått og rått medlemmer med Mike Farris.",
+    featured: true,
+  },
+  // NON-FEATURED
+  {
+    src: "./bilder/bogr-ankomst.jpg",
+    alt: "Blått og rått ankommer Svalbard.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-band-bilde.jpg",
+    alt: "Blått og rått bandbilde.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-kjetil-gom.jpg",
+    alt: "Blått og rått Kjetil og GOM.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-medlemmer-mike-farris.jpg",
+    alt: "Blått og rått med Mike Farris.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-signering-gom.jpg",
+    alt: "Blått og rått platesignering GOM.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-signering-ronald.jpg",
+    alt: "Blått og rått platesignering Ronald.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-signering-svein-knut.jpg",
+    alt: "Blått og rått platesignering Svein og Knut.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-studio-band.jpg",
+    alt: "Blått og rått i studio.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-studio-gom.jpg",
+    alt: "Blått og rått i studio, GOM.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-studio-kjetil.jpg",
+    alt: "Blått og rått i studio, Kjetil.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-studio-ronald.jpg",
+    alt: "Blått og rått i studio, Ronald.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-studio-svein.jpg",
+    alt: "Blått og rått i studio, Svein.",
+    featured: false,
+  },
+  {
+    src: "./bilder/bogr-studio.jpg",
+    alt: "Blått og rått i studio.",
+    featured: false,
   },
 ];
