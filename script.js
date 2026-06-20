9774d2d24855225af7005b76630a3576d5188b3b
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-06-20 16:05:09 +0200

    Moved eventListener inside the function and created one for showing images clicked in the strip aswell

--- script.js
+++ script.js
@@ -70,22 +70,127 @@ displayImages();
 // Display images (Images page)
 
 // Display clicked image
+function displayImage(id, index) {
+  const container = document.querySelector(".image-showcase-container");
+  if (!container) return;
+  container.innerHTML = "";
 
-const imageLink = document.querySelector(".images-container a");
+  // Showcase header
+  const headerDiv = document.createElement("div");
+  headerDiv.classList.add("showcase-header");
+
+  const title = document.createElement("h3");
+  title.classList.add("title");
+  title.textContent = id.alt;
+
+  const closeBtn = document.createElement("button");
+  closeBtn.classList.add("close-btn");
+  closeBtn.setAttribute("aria-label", "Close button");
+
+  const closeIcon = document.createElement("i");
+  closeIcon.classList.add("fa-solid", "fa-x");
+
+  //Showcase main img
+  const mainContainer = document.createElement("div");
+  mainContainer.classList.add("main-image-container");
+
+  const mainImg = document.createElement("img");
+  mainImg.src = id.src;
+  mainImg.alt = id.alt;
+
+  // Next-prev buttons
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.classList.add("prev-next-btn");
+
+  const prevBtn = document.createElement("button");
+  prevBtn.setAttribute("aria-label", "Previous image");
+  prevBtn.classList.add("prev");
+
+  const prevBtnIcon = document.createElement("i");
+  prevBtnIcon.classList.add("fa-solid", "fa-chevron-left");
+
+  const nextBtn = document.createElement("button");
+  nextBtn.setAttribute("aria-label", "Next image");
+  nextBtn.classList.add("next");
+
+  const nextBtnIcon = document.createElement("i");
+  nextBtnIcon.classList.add("fa-solid", "fa-chevron-right");
+
+  // Strip
+  const stripContainer = document.createElement("div");
+  stripContainer.classList.add("preview-strip-container");
+
+  const prewPrevBtn = document.createElement("button");
+  prewPrevBtn.classList.add("prew-prev");
+  prewPrevBtn.setAttribute("aria-label", "Previous part");
+
+  const prewPrevBtnIcon = document.createElement("i");
+  prewPrevBtnIcon.classList.add("fa-solid", "fa-chevron-left");
+
+  const prewNextBtn = document.createElement("button");
+  prewNextBtn.setAttribute("aria-label", "Next part");
+  prewNextBtn.classList.add("prew-next");
+
+  const prewNextBtnIcon = document.createElement("i");
+  prewNextBtnIcon.classList.add("fa-solid", "fa-chevron-right");
+
+  const strip = document.createElement("div");
+  strip.classList.add("strip");
+
+  container.appendChild(headerDiv);
+  headerDiv.appendChild(title);
+  headerDiv.appendChild(closeBtn);
+  closeBtn.appendChild(closeIcon);
+  container.appendChild(mainContainer);
+  mainContainer.appendChild(mainImg);
+  mainContainer.appendChild(buttonsDiv);
+  buttonsDiv.appendChild(prevBtn);
+  prevBtn.appendChild(prevBtnIcon);
+  buttonsDiv.appendChild(nextBtn);
+  nextBtn.appendChild(nextBtnIcon);
+  container.appendChild(stripContainer);
+  stripContainer.appendChild(prewPrevBtn);
+  prewPrevBtn.appendChild(prewPrevBtnIcon);
+  stripContainer.appendChild(prewNextBtn);
+  prewNextBtn.appendChild(prewNextBtnIcon);
+  stripContainer.appendChild(strip);
+
+  images.forEach((image) => {
+    const imgA = document.createElement("a");
+    const img = document.createElement("img");
+    img.src = image.src;
+    img.alt = image.alt;
+    img.dataset.id = image.id;
+    strip.appendChild(imgA);
+    imgA.appendChild(img);
+  });
+  closeBtn.addEventListener("click", () => {
+    imageContainer.style.display = "none";
+  });
+  const prewImg = document.querySelectorAll(".strip a");
+  prewImg.forEach((i) => {
+    i.addEventListener("click", (event) => {
+      const img = event.target.dataset.id;
+      const imageToShow = images.find((image) => image.id === img);
+      // Find image index
+      const imageIndex = images.findIndex((place) => place.id === img);
+      displayImage(imageToShow, imageIndex);
+    });
+  });
+}
+
+const imageLink = document.querySelectorAll(".images-container a");
 const imageContainer = document.querySelector(".image-showcase-container");
-imageLink.addEventListener("click", (event) => {
-  const img = event.target.dataset.id;
-  if (!img) return;
-  imageContainer.style.display = "flex";
-  if (!imageContainer) return;
-  // Find id of image to show
-  const imageToShow = images.find((image) => image.id === img);
-  // Find image index
-  const imageIndex = images.findIndex((place) => place.id === img);
-  displayImage(imageToShow, imageIndex);
-});
-
-const closeBtn = document.querySelector(".close-btn");
-closeBtn.addEventListener("click", () => {
-  imageContainer.style.display = "none";
+imageLink.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const img = event.target.dataset.id;
+    if (!img) return;
+    imageContainer.style.display = "flex";
+    if (!imageContainer) return;
+    // Find id of image to show
+    const imageToShow = images.find((image) => image.id === img);
+    // Find image index
+    const imageIndex = images.findIndex((place) => place.id === img);
+    displayImage(imageToShow, imageIndex);
+  });
 });
