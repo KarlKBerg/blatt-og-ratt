5bf1bba5f1492d99c0ab1d559bb4a66be5da9d3c
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-07-01 18:16:40 +0200

    Removed old concert

--- js/concerts.js
+++ js/concerts.js
@@ -1,12 +1,5 @@
 export let concerts = [
   {
-    date: "2026-06-20",
-    displayDate: "20. Juni 2026",
-    name: "Lyngenfjord live",
-    place: "Lyngen",
-    tickets: "https://tikkio.com/events/62885-lyngenfjord-live-ivgobahta",
-  },
-  {
     date: "25. juni 2026",
     name: "Finnsnes i fest",
     place: "Finnsnes",
