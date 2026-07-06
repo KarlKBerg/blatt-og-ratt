7106c6aabbbabe321207a94a3b485a60e0f0000b
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-07-06 22:00:00 +0200

    Removed display images and display all images functions to separate file, imported from image-functions.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 "use strict";
 import { concerts } from "./js/concerts.js";
-import { images } from "./js/images.js";
 import { singles } from "./js/singles.js";
 import { albums } from "./js/albums.js";
+import { displayImages, displayAllImages } from "./js/image-functions.js";
 let date = new Date();
 let basePath = "./";
 if (window.location.pathname.includes("sider")) {
@@ -55,73 +55,9 @@ function displayConcerts() {
 }
 displayConcerts();
 
-// Display images (Landing page)
-function displayImages() {
-  const container = document.querySelector(".images-container");
-  if (!container) return;
-  container.innerHTML = "";
+displayImages(basePath);
 
-  images.forEach((i) => {
-    if (i.featured) {
-      const imageA = document.createElement("a");
-      const image = document.createElement("img");
-      image.src = basePath + i.src;
-      image.alt = i.alt;
-      image.dataset.id = i.id;
-      container.appendChild(imageA);
-      imageA.appendChild(image);
-    }
-  });
-}
-displayImages();
-
-// Display images (Gallery page)
-function displayAllImages() {
-  let imgPosition = 0;
-  const container = document.querySelector(".gallery-container");
-  if (!container) return;
-  container.innerHTML = "";
-
-  // Show first image in showcase
-  const showcase = document.querySelector(".gallery-showcase");
-  if (!showcase) return;
-  showcase.innerHTML = "";
-
-  const showcaseImage = document.createElement("img");
-  showcaseImage.src = basePath + images[imgPosition].src;
-  showcaseImage.alt = images[imgPosition].alt;
-  showcase.appendChild(showcaseImage);
-
-  // Load all images in grid
-  images.forEach((image) => {
-    const imgContainer = document.createElement("a");
-    const img = document.createElement("img");
-    img.src = basePath + image.src;
-    img.alt = image.alt;
-    img.dataset.id = image.id;
-
-    container.appendChild(imgContainer);
-    imgContainer.appendChild(img);
-  });
-  // Get ID of clicked image
-  container.querySelectorAll("a").forEach((i) => {
-    i.addEventListener("click", (event) => {
-      let imgId = event.target.dataset.id;
-      const position = images.findIndex((imageID) => imageID.id === imgId);
-      imgPosition = position;
-      // Show clicked image in showcase
-      showcaseImage.src = basePath + images[imgPosition].src;
-      showcaseImage.alt = images[imgPosition].alt;
-      showcase.appendChild(showcaseImage);
-      // Scroll to top
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    });
-  });
-}
-displayAllImages();
+displayAllImages(basePath);
 // Display next concert
 function nextConcert() {
   const container = document.querySelector(".next-concert");
