2250de7f27132d5521762480b3715de174d15c8e
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-07-23 06:53:29 +0200

    Imported displayProduct function, created ID and product vars and ran displayProduct with safeguard

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 // Imports
+import { products } from "./js/data/products.js";
 import {
   displaySingles,
   displayAlbums,
@@ -13,11 +14,14 @@ import {
 import { displayConcerts } from "./js/functions/concert-functions.js";
 import { initNav } from "./js/functions/nav-functions.js";
 import { displayProducts } from "./js/functions/shop-functions.js";
+import { renderProduct } from "./js/functions/product-functions.js";
+
 let basePath = "./";
 if (window.location.pathname.includes("sider")) {
   basePath = "../";
 }
-
+const ID = new URLSearchParams(window.location.search).get("id");
+const product = products.find((p) => p.id === ID);
 // Call functions
 displayConcerts();
 displayImages(basePath);
@@ -28,3 +32,4 @@ musicNav();
 initNav();
 modalListeners();
 displayProducts(basePath);
+if (product) renderProduct(product, basePath);
