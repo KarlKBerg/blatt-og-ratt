8c7a0bf8325eefbf9446482e24a7c9d26c652b8b
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-07-23 06:52:34 +0200

    Created function to display product based on id

---
+++ js/functions/product-functions.js
@@ -0,0 +1,56 @@
+"use strict";
+import { products } from "../data/products.js";
+export function renderProduct(product, path) {
+  const container = document.querySelector(".product-container");
+  if (!container) return;
+  container.innerHTML = "";
+
+  const image = document.createElement("img");
+  image.src = path + product.img;
+  image.alt = product.info;
+
+  const descriptionContainer = document.createElement("div");
+  descriptionContainer.classList.add("product-description-container");
+
+  const title = document.createElement("h2");
+  title.classList.add("product-title");
+  title.textContent = product.name;
+
+  const description = document.createElement("p");
+  description.classList.add("product-description");
+  description.textContent = product.info;
+
+  const songList = document.createElement("ul");
+
+  const buyContainer = document.createElement("div");
+  buyContainer.classList.add("buy-container");
+
+  const qrCode = document.createElement("img");
+  qrCode.src = path + "bilder/vipps-butikk.jpg";
+
+  const email = document.createElement("a");
+  email.href = "mailto:blattogratt@gmail.com";
+  email.textContent = "blattogratt@gmail.com";
+
+  const disclaimer = document.createElement("p");
+  disclaimer.classList.add("disclaimer");
+  disclaimer.append =
+    "For øyeblikket er det bare mulig å handle fra vår butikk i vipps. Vi beklager for eventuelle ulemper dette kan medføre. Om du har spørsmål, eller ikke har mulighet til å handle via vipps, vennligst kontakt oss ved å sende mail til " +
+    email;
+
+  container.appendChild(image);
+  container.appendChild(descriptionContainer);
+  descriptionContainer.appendChild(title);
+  descriptionContainer.appendChild(description);
+  if (product.songs?.length) {
+    descriptionContainer.appendChild(songList);
+    product.songs.forEach((s) => {
+      const song = document.createElement("li");
+      song.textContent = s;
+      songList.appendChild(song);
+    });
+  }
+  descriptionContainer.appendChild(buyContainer);
+  buyContainer.appendChild(qrCode);
+  buyContainer.appendChild(disclaimer);
+}
